5886d843f63812e012a89a37c75354df6d390e2f
Andrey <funikov.1997@mail.ru>
2021-03-18 23:30:15 +0300

    Урок №2. Изучил понятие свойств класса

--- classes/FirstClass.php
+++ classes/FirstClass.php
@@ -1,5 +1,12 @@
 <?php
 
-class MyFirstClass {
+class Car {
+    /*Область видимости*/
+
+    public $name =  4; //Свойство автомобиля как объекта - имя
+    public $color = 'red'; //Цвет автомобиля
+    public $brand   ; //Марка
+    public $year = 2020   ; //Год
+
 
 }
--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 require_once 'classes/FirstClass.php';
 
-var_dump($object = new MyFirstClass());
+$object = new Car();
+
+$car1 = new Car();
+
+function debug($data) {
+    echo '<pre>'.print_r($data,1).'</pre>';
+}
+debug($car1);
+
+$car1->color = 'чёрный';
+$car1->brand = 'mustang';
+
+echo '<h3>О моём автомобиле:</h3><br>';
+echo "Марка: $car1->brand<br>";
+echo "ГВ: $car1->year<br>";
+echo "Цвет: $car1->color<br>";
